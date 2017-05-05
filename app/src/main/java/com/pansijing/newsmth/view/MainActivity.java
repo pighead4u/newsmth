b1e0567d386df30020594325e74c13f808597cb7
zhuhuanhuan <zhuhuanhuan@qccr.com>
2017-05-05 17:14:16 +0800

    delete:无用的dependency

--- app/src/main/java/com/pansijing/newsmth/view/MainActivity.java
+++ app/src/main/java/com/pansijing/newsmth/view/MainActivity.java
@@ -3,11 +3,9 @@ package com.pansijing.newsmth.view;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 import com.pansijing.newsmth.R;
 import com.pansijing.newsmth.presentor.MainPresentor;
 
@@ -15,20 +13,27 @@ public class MainActivity extends AppCompatActivity implements IMain {
   private static final String TAG = "MainActivity";
 
   MainPresentor mMainPresentor;
-  @BindView(R.id.btn) Button btn;
-  @BindView(R.id.content) TextView tvContent;
+  Button btn;
+  TextView tvContent;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    ButterKnife.bind(this);
 
+    initView();
     mMainPresentor = new MainPresentor(this);
   }
 
-  @OnClick(R.id.btn) void fetchTopData() {
-    Log.e(TAG, "fetchTopData: ");
-    mMainPresentor.getTop10();
+  void initView() {
+    btn = (Button) findViewById(R.id.btn);
+    tvContent = (TextView) findViewById(R.id.content);
+
+    btn.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        Log.e(TAG, "fetchTopData: ");
+        mMainPresentor.getTop10();
+      }
+    });
   }
 
   @Override public void getTop10Success(String data) {
@@ -37,8 +42,5 @@ public class MainActivity extends AppCompatActivity implements IMain {
 
   @Override public void getTop10Failed(String data) {
     tvContent.setText(data);
-  }
-
-  @OnClick(R.id.btn) public void onViewClicked() {
   }
 }
